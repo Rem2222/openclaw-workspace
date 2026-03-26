eedacada21dfd05d80c7c2453202d5e5abeb7e7c
Rem2222 <218647427+Rem2222@users.noreply.github.com>
2026-03-26 23:24:11 +0300

    feat: initial project setup with Phaser 3 + Vite

---
+++ projects/Campfire-Survival-Superpowers/src/config/constants.js
@@ -0,0 +1,22 @@
+export const GAME_WIDTH = 1280;
+export const GAME_HEIGHT = 720;
+
+export const PLAYER_SPEED = 200;
+export const PLAYER_MAX_HP = 100;
+export const PLAYER_ATTACK_DAMAGE = 10;
+export const PLAYER_ATTACK_RANGE = 50;
+export const PLAYER_ATTACK_COOLDOWN = 500;
+
+export const CAMPFIRE_MAX_HP = 100;
+export const CAMPFIRE_X = GAME_WIDTH / 2;
+export const CAMPFIRE_Y = GAME_HEIGHT / 2;
+export const CAMPFIRE_LIGHT_RADIUS = 150;
+
+export const TREE_CHOP_TIME = 2000;
+export const TREE_COUNT = 15;
+export const TREE_SPAWN_MIN_DISTANCE = 200;
+
+export const LOG_HEAL_VALUE = 10;
+export const LOG_PICKUP_RANGE = 30;
+
+export const MONSTER_SPAWN_MARGIN = 100;
---
+++ projects/Campfire-Survival-Superpowers/src/main.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  parent: 'game-container',
+  backgroundColor: '#1a1a2e',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [] // Will add scenes later
+};
+
+const game = new Phaser.Game(config);
+
+export default game;
